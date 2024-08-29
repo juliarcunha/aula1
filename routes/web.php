c68bac6871322e03b2780392f101d47d0c9d75cb
juliarcunha <juliarodriguesc13@gmail.com>
2024-08-29 10:26:00 -0300

    criacao do projeto e minha tela para teste

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/tela', function () {
+    return view('minha-tela');
+});
+
+Route::get('/nome/{nome}', 
+function (string $nome) {
+    return 'Olá '.$nome;
+});
+
+Route::get('/nome/{nome}/{profissao}', 
+function (string $nome, string $profissao) {
+    return "Olá $nome, você é a melhor $profissao do mundo!";
+});
